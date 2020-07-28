c640684691635421333e11d1431b2409159f73cd
carrieprice78 <carrieprice78@gmail.com>
2020-07-28 18:23:24 -0400

    spacing on banner and padding

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,330 +6,336 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#wrapper'),
-		$header = $('#header'),
-		$banner = $('#banner');
+    var $window = $(window),
+        $body = $('body'),
+        $wrapper = $('#wrapper'),
+        $header = $('#header'),
+        $banner = $('#banner');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: ['481px', '736px'],
+        xsmall: ['361px', '480px'],
+        xxsmall: [null, '360px']
+    });
 
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = (browser.name == 'ie' || browser.name == 'edge' || browser.mobile) ? function() { return $(this) } : function(intensity) {
+    /**
+     * Applies parallax scrolling to an element's background image.
+     * @return {jQuery} jQuery object.
+     */
+    $.fn._parallax = (browser.name == 'ie' || browser.name == 'edge' || browser.mobile) ? function() { return $(this) } : function(intensity) {
 
-		var	$window = $(window),
-			$this = $(this);
+        var $window = $(window),
+            $this = $(this);
 
-		if (this.length == 0 || intensity === 0)
-			return $this;
+        if (this.length == 0 || intensity === 0)
+            return $this;
 
-		if (this.length > 1) {
+        if (this.length > 1) {
 
-			for (var i=0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
+            for (var i = 0; i < this.length; i++)
+                $(this[i])._parallax(intensity);
 
-			return $this;
+            return $this;
 
-		}
+        }
 
-		if (!intensity)
-			intensity = 0.25;
+        if (!intensity)
+            intensity = 0.25;
 
-		$this.each(function() {
+        $this.each(function() {
 
-			var $t = $(this),
-				on, off;
+            var $t = $(this),
+                on, off;
 
-			on = function() {
+            on = function() {
 
-				$t.css('background-position', 'center 100%, center 100%, center 0px');
+                $t.css('background-position', 'center 100%, center 100%, center 0px');
 
-				$window
-					.on('scroll._parallax', function() {
+                $window
+                    .on('scroll._parallax', function() {
 
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+                        var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
+                        $t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
 
-					});
+                    });
 
-			};
+            };
 
-			off = function() {
+            off = function() {
 
-				$t
-					.css('background-position', '');
+                $t
+                    .css('background-position', '');
 
-				$window
-					.off('scroll._parallax');
+                $window
+                    .off('scroll._parallax');
 
-			};
+            };
 
-			breakpoints.on('<=medium', off);
-			breakpoints.on('>medium', on);
+            breakpoints.on('<=medium', off);
+            breakpoints.on('>medium', on);
 
-		});
+        });
 
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
-				$window.trigger('scroll');
-			});
+        $window
+            .off('load._parallax resize._parallax')
+            .on('load._parallax resize._parallax', function() {
+                $window.trigger('scroll');
+            });
 
-		return $(this);
+        return $(this);
 
-	};
+    };
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Clear transitioning state on unload/hide.
-		$window.on('unload pagehide', function() {
-			window.setTimeout(function() {
-				$('.is-transitioning').removeClass('is-transitioning');
-			}, 250);
-		});
+    // Clear transitioning state on unload/hide.
+    $window.on('unload pagehide', function() {
+        window.setTimeout(function() {
+            $('.is-transitioning').removeClass('is-transitioning');
+        }, 250);
+    });
 
-	// Fix: Enable IE-only tweaks.
-		if (browser.name == 'ie' || browser.name == 'edge')
-			$body.addClass('is-ie');
+    // Fix: Enable IE-only tweaks.
+    if (browser.name == 'ie' || browser.name == 'edge')
+        $body.addClass('is-ie');
 
-	// Scrolly.
-		$('.scrolly').scrolly({
-			offset: function() {
-				return $header.height() - 2;
-			}
-		});
+    // Scrolly.
+    $('.scrolly').scrolly({
+        offset: function() {
+            return $header.height() - 2;
+        }
+    });
 
-	// Tiles.
-		var $tiles = $('.tiles > article');
+    // Tiles.
+    var $tiles = $('.tiles > article');
 
-		$tiles.each(function() {
+    $tiles.each(function() {
 
-			var $this = $(this),
-				$image = $this.find('.image'), $img = $image.find('img'),
-				$link = $this.find('.link'),
-				x;
+        var $this = $(this),
+            $image = $this.find('.image'),
+            $img = $image.find('img'),
+            $link = $this.find('.link'),
+            x;
 
-			// Image.
+        // Image.
 
-				// Set image.
-					$this.css('background-image', 'url(' + $img.attr('src') + ')');
+        // Set image.
+        $this.css('background-image', 'url(' + $img.attr('src') + ')');
 
-				// Set position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
+        // Set position.
+        if (x = $img.data('position'))
+            $image.css('background-position', x);
 
-				// Hide original.
-					$image.hide();
+        // Hide original.
+        $image.hide();
 
-			// Link.
-				if ($link.length > 0) {
+        // Link.
+        if ($link.length > 0) {
 
-					$x = $link.clone()
-						.text('')
-						.addClass('primary')
-						.appendTo($this);
+            $x = $link.clone()
+                .text('')
+                .addClass('primary')
+                .attr('aria-label', 'navigation')
+                .appendTo($this);
 
-					$link = $link.add($x);
+            $link = $link.add($x);
 
-					$link.on('click', function(event) {
+            $link.on('click', function(event) {
 
-						var href = $link.attr('href');
+                var href = $link.attr('href');
 
-						// Prevent default.
-							event.stopPropagation();
-							event.preventDefault();
+                // Prevent default.
+                event.stopPropagation();
+                event.preventDefault();
 
-						// Target blank?
-							if ($link.attr('target') == '_blank') {
+                // Target blank?
+                if ($link.attr('target') == '_blank') {
 
-								// Open in new tab.
-									window.open(href);
+                    // Open in new tab.
+                    window.open(href);
 
-							}
+                }
 
-						// Otherwise ...
-							else {
+                // Otherwise ...
+                else {
 
-								// Start transitioning.
-									$this.addClass('is-transitioning');
-									$wrapper.addClass('is-transitioning');
+                    // Start transitioning.
+                    $this.addClass('is-transitioning');
+                    $wrapper.addClass('is-transitioning');
 
-								// Redirect.
-									window.setTimeout(function() {
-										location.href = href;
-									}, 500);
+                    // Redirect.
+                    window.setTimeout(function() {
+                        location.href = href;
+                    }, 500);
 
-							}
+                }
 
-					});
+            });
 
-				}
+        }
 
-		});
+    });
 
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
+    // Header.
+    if ($banner.length > 0 &&
+        $header.hasClass('alt')) {
 
-			$window.on('resize', function() {
-				$window.trigger('scroll');
-			});
+        $window.on('resize', function() {
+            $window.trigger('scroll');
+        });
 
-			$window.on('load', function() {
+        $window.on('load', function() {
 
-				$banner.scrollex({
-					bottom:		$header.height() + 10,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-				});
+            $banner.scrollex({
+                bottom: $header.height() + 10,
+                terminate: function() { $header.removeClass('alt'); },
+                enter: function() { $header.addClass('alt'); },
+                leave: function() {
+                    $header.removeClass('alt');
+                    $header.addClass('reveal');
+                }
+            });
 
-				window.setTimeout(function() {
-					$window.triggerHandler('scroll');
-				}, 100);
+            window.setTimeout(function() {
+                $window.triggerHandler('scroll');
+            }, 100);
 
-			});
+        });
 
-		}
+    }
 
-	// Banner.
-		$banner.each(function() {
+    // Banner.
+    $banner.each(function() {
 
-			var $this = $(this),
-				$image = $this.find('.image'), $img = $image.find('img');
+        var $this = $(this),
+            $image = $this.find('.image'),
+            $img = $image.find('img');
 
-			// Parallax.
-				$this._parallax(0.275);
+        // Parallax.
+        $this._parallax(0.275);
 
-			// Image.
-				if ($image.length > 0) {
+        // Image.
+        if ($image.length > 0) {
 
-					// Set image.
-						$this.css('background-image', 'url(' + $img.attr('src') + ')');
+            // Set image.
+            $this.css('background-image', 'url(' + $img.attr('src') + ')');
 
-					// Hide original.
-						$image.hide();
+            // Hide original.
+            $image.hide();
 
-				}
+        }
 
-		});
+    });
 
-	// Menu.
-		var $menu = $('#menu'),
-			$menuInner;
+    // Menu.
+    var $menu = $('#menu'),
+        $menuInner;
 
-		$menu.wrapInner('<div class="inner"></div>');
-		$menuInner = $menu.children('.inner');
-		$menu._locked = false;
+    $menu.wrapInner('<div class="inner"></div>');
+    $menuInner = $menu.children('.inner');
+    $menu._locked = false;
 
-		$menu._lock = function() {
+    $menu._lock = function() {
 
-			if ($menu._locked)
-				return false;
+        if ($menu._locked)
+            return false;
 
-			$menu._locked = true;
+        $menu._locked = true;
 
-			window.setTimeout(function() {
-				$menu._locked = false;
-			}, 350);
+        window.setTimeout(function() {
+            $menu._locked = false;
+        }, 350);
 
-			return true;
+        return true;
 
-		};
+    };
 
-		$menu._show = function() {
+    $menu._show = function() {
 
-			if ($menu._lock())
-				$body.addClass('is-menu-visible');
+        if ($menu._lock())
+            $body.addClass('is-menu-visible');
 
-		};
+    };
 
-		$menu._hide = function() {
+    $menu._hide = function() {
 
-			if ($menu._lock())
-				$body.removeClass('is-menu-visible');
+        if ($menu._lock())
+            $body.removeClass('is-menu-visible');
 
-		};
+    };
 
-		$menu._toggle = function() {
+    $menu._toggle = function() {
 
-			if ($menu._lock())
-				$body.toggleClass('is-menu-visible');
+        if ($menu._lock())
+            $body.toggleClass('is-menu-visible');
 
-		};
+    };
 
-		$menuInner
-			.on('click', function(event) {
-				event.stopPropagation();
-			})
-			.on('click', 'a', function(event) {
+    $menuInner
+        .on('click', function(event) {
+            event.stopPropagation();
+        })
+        .on('click', 'a', function(event) {
 
-				var href = $(this).attr('href');
+            var href = $(this).attr('href');
 
-				event.preventDefault();
-				event.stopPropagation();
+            event.preventDefault();
+            event.stopPropagation();
 
-				// Hide.
-					$menu._hide();
+            // Hide.
+            $menu._hide();
 
-				// Redirect.
-					window.setTimeout(function() {
-						window.location.href = href;
-					}, 250);
+            // Redirect.
+            window.setTimeout(function() {
+                window.location.href = href;
+            }, 250);
 
-			});
+        });
 
-		$menu
-			.appendTo($body)
-			.on('click', function(event) {
+    $menu
+        .appendTo($body)
+        .on('click', function(event) {
 
-				event.stopPropagation();
-				event.preventDefault();
+            event.stopPropagation();
+            event.preventDefault();
 
-				$body.removeClass('is-menu-visible');
+            $body.removeClass('is-menu-visible');
 
-			})
-			.append('<a class="close" href="#menu">Close</a>');
+        })
+        .append('<a class="close" href="#menu">Close</a>');
 
-		$body
-			.on('click', 'a[href="#menu"]', function(event) {
+    $body
+        .on('click', 'a[href="#menu"]', function(event) {
 
-				event.stopPropagation();
-				event.preventDefault();
+            event.stopPropagation();
+            event.preventDefault();
 
-				// Toggle.
-					$menu._toggle();
+            // Toggle.
+            $menu._toggle();
 
-			})
-			.on('click', function(event) {
+        })
+        .on('click', function(event) {
 
-				// Hide.
-					$menu._hide();
+            // Hide.
+            $menu._hide();
 
-			})
-			.on('keydown', function(event) {
+        })
+        .on('keydown', function(event) {
 
-				// Hide on escape.
-					if (event.keyCode == 27)
-						$menu._hide();
+            // Hide on escape.
+            if (event.keyCode == 27)
+                $menu._hide();
 
-			});
+        });
 
 })(jQuery);
